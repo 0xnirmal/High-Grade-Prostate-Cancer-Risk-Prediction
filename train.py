06daf745c3ff150dbc7438da6fd6aaa7d63dd2c9
Nirmal Krishnan <nirmal@Nirmals-MacBook-Pro.local>
2017-05-01 23:52:15 -0400

    updated training file

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import cross_val_score
 from sklearn import svm
 from sklearn import linear_model
 from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 try:
 	data = sys.argv[1]
@@ -18,6 +19,26 @@ labels_path = os.getcwd() + "/" + data + "/labels.csv"
 
 raw_data_matrix = np.genfromtxt(data_path, skip_header=1, delimiter=',', usecols=range(1, 19906))
 labels = np.genfromtxt(labels_path, delimiter=',', usecols=1)
+
+clf = LinearDiscriminantAnalysis()
+scores = cross_val_score(clf, raw_data_matrix, labels, cv=5)
+print("LDA w/ no DR")
+print(scores)
+
+clf = LinearDiscriminantAnalysis(n_components=10000)
+scores = cross_val_score(clf, raw_data_matrix, labels, cv=5)
+print("LDA w/ DR=10000")
+print(scores)
+
+clf = LinearDiscriminantAnalysis(n_components=1000)
+scores = cross_val_score(clf, raw_data_matrix, labels, cv=5)
+print("LDA w/ DR=1000")
+print(scores)
+
+clf = LinearDiscriminantAnalysis(n_components=100)
+scores = cross_val_score(clf, raw_data_matrix, labels, cv=5)
+print("LDA w/ DR=100")
+print(scores)
 
 clf = svm.SVC(kernel='linear', C=1)
 scores = cross_val_score(clf, raw_data_matrix, labels, cv=5)
